611aee2e704ffbad8579e5005ca36232097f96c5
HellPie <hellpiedev+git@gmail.com>
2017-10-16 21:41:45 +0200

    Fix issue with enum not accepting null

--- bot/utils.py
+++ bot/utils.py
@@ -6,7 +6,7 @@ class OpStatus(IntEnum):
 	SUCCESS = 0x2ECC71,
 	FAILURE = 0xc0392B,
 	WARNING = 0xf39C12,
-	NONE = None
+	NONE = -1
 
 
 def build_embed(ctx, desc: str, title: str = '', status: OpStatus = OpStatus.SUCCESS) -> Embed:
@@ -14,7 +14,7 @@ def build_embed(ctx, desc: str, title: str = '', status: OpStatus = OpStatus.SUC
 	embed = Embed(
 		title=title,
 		description=desc,
-		color=status.value if status is not None else OpStatus.WARNING
+		color=status.value if status is not None and status is not -1 else None if status is -1 else OpStatus.WARNING
 	)
 	embed.set_author(name=name, icon_url=ctx.bot.user.avatar_url)
 	return embed
