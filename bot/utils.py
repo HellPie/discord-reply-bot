a2d4381de5dc50110a0e57d7e56a668edbee2ccf
HellPie <hellpiedev+git@gmail.com>
2017-10-16 13:41:31 +0200

    Add support for colored output in embeds

--- bot/utils.py
+++ bot/utils.py
@@ -1,11 +1,19 @@
+from enum import IntEnum
 from discord import Embed
 
 
-def build_embed(ctx, desc: str, title: str = ''):
+class OpStatus(IntEnum):
+	SUCCESS = 0x2ECC71,
+	FAILURE = 0xc0392B,
+	WARNING = 0xf39C12
+
+
+def build_embed(ctx, desc: str, title: str = '', status: OpStatus = OpStatus.SUCCESS) -> Embed:
 	name = ctx.message.server.me.nick if ctx.message.server.me.nick is not None else ctx.bot.user.name
 	embed = Embed(
 		title=title,
-		description=desc
+		description=desc,
+		color=status.value if status is not None else OpStatus.WARNING
 	)
 	embed.set_author(name=name, icon_url=ctx.bot.user.avatar_url)
 	return embed
